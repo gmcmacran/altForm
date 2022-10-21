77863415a25faa450bd4f019c045dcb646acf4a6
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-10-21 18:58:43 -0500

    add bernoulli

--- R/a_function_factories.R
+++ R/a_function_factories.R
@@ -2,7 +2,7 @@
 
 # fix check.
 # Not actually global.
-utils::globalVariables(c("x", "mu", "sigma", "log"))
+utils::globalVariables(c("x", "mu", "sigma", "log", "size"))
 
 #' @keywords internal
 #' A function factory that returns pdfs
@@ -190,6 +190,151 @@ create_pdf_exponential_form <- function(a, b, c2, link, calc_phi, hasNuisance, X
   })
 
   exec_globals <- list(a = a, b = b, c2 = c2, link = link, calc_phi = calc_phi, XLB = XLB, XUB = XUB, MLB = MLB, MUB = MUB)
+  exec_env <- rlang::new_environment(data = exec_globals, parent = rlang::base_env())
+
+  f <- rlang::new_function(args, body, env = exec_env)
+
+  return(f)
+}
+
+#' @keywords internal
+#' A function factory that returns exponential pdfs w/ size parameter (binomial and negative binomial)
+create_pdf_exponential_form_with_size <- function(a, b, c2, link, calc_phi, XLB) {
+  force(a)
+  if (!inherits(a, "function")) {
+    stop("Argument a must be a function.")
+  }
+  args <- names(formals(a))
+  if (args[1] != "phi") {
+    stop("a's first argument is not phi.")
+  }
+  if (length(args) != 1) {
+    stop("a has too many arguments.")
+  }
+  rm(args)
+
+  force(b)
+  if (!inherits(b, "function")) {
+    stop("Argument b must be a function.")
+  }
+  args <- names(formals(b))
+  if (args[1] != "theta") {
+    stop("b's first argument is not theta.")
+  }
+  if (args[2] != "size") {
+    stop("b's second argument is not size.")
+  }
+  if (length(args) != 2) {
+    stop("b has too many arguments.")
+  }
+  rm(args)
+
+  force(c2)
+  if (!inherits(c2, "function")) {
+    stop("Argument c2 must be a function.")
+  }
+  args <- names(formals(c2))
+  if (args[1] != "x") {
+    stop("c2's first argument is not x.")
+  }
+  if (args[2] != "size") {
+    stop("c2's first argument is not size.")
+  }
+  if (length(args) != 2) {
+    stop("c2 has too many arguments.")
+  }
+  rm(args)
+
+  force(link)
+  if (!inherits(link, "function")) {
+    stop("Argument link must be a function.")
+  }
+  args <- names(formals(link))
+  if (args[1] != "mu") {
+    stop("link's first argument is not mu.")
+  }
+  if (length(args) != 1) {
+    stop("link has too many arguments.")
+  }
+  rm(args)
+
+  force(calc_phi)
+  if (!inherits(calc_phi, "function")) {
+    stop("Argument calc_phi must be a function.")
+  }
+  args <- names(formals(calc_phi))
+  if (length(args) > 2) {
+    stop("calc_phi has too many arguments.")
+  }
+  rm(args)
+
+  force(XLB)
+  if (!is.numeric(XLB)) {
+    stop("Argument XLB should be numeric.")
+  }
+  if (length(XLB) != 1) {
+    stop("Argument XLB should have length one.")
+  }
+
+  args <- rlang::pairlist2(x = , mu = , size = , log = FALSE)
+
+  body <- rlang::expr({
+    # check inputs
+    if (length(x) < 1) {
+      stop("Argument x must have positive length.")
+    }
+    if (!is.numeric(x)) {
+      stop("Argument x must be numeric.")
+    }
+    if (any(x < XLB)) {
+      stop(paste("All elements in x must be greater than or equal to ", XLB, sep = ""))
+    }
+
+    if (length(mu) != 1) {
+      stop("Argument mu must have length one.")
+    }
+    if (!is.numeric(mu)) {
+      stop("Argument mu must be numeric.")
+    }
+    if (mu <= 0) {
+      stop("Argument mu must be greater than zero.")
+    }
+    if (mu >= 1) {
+      stop("Argument mu must be less than one.")
+    }
+
+    if (length(size) != 1) {
+      stop("Argument size must have length one.")
+    }
+    if (!is.numeric(size)) {
+      stop("Argument size must be numeric.")
+    }
+    if (size <= 0) {
+      stop("Argument size must be greater than 0.")
+    }
+    if (size >= 10000) {
+      stop("Argument size must be less than 10,000.")
+    }
+
+    if (length(log) != 1) {
+      stop("Argument log must have length one.")
+    }
+    if (!is.logical(log)) {
+      stop("Argument log must be logical.")
+    }
+
+    theta <- link(mu)
+    phi <- calc_phi()
+
+    p <- (x * theta - b(theta, size)) / a(phi) + c2(x, size)
+    if (!log) {
+      p <- exp(p)
+    }
+
+    return(p)
+  })
+
+  exec_globals <- list(a = a, b = b, c2 = c2, link = link, calc_phi = calc_phi, XLB = XLB)
   exec_env <- rlang::new_environment(data = exec_globals, parent = rlang::base_env())
 
   f <- rlang::new_function(args, body, env = exec_env)
--- R/binomial.R
+++ R/binomial.R
@@ -4,13 +4,13 @@ a <- function(phi) {
 }
 
 #' @keywords internal
-b <- function(theta) {
-  return(log(1 + exp(theta)))
+b <- function(theta, size) {
+  return(size * log(1 + exp(theta)))
 }
 
 #' @keywords internal
-c2 <- function(x, phi) {
-  out <- lchoose(1, 1 * x)
+c2 <- function(x, size) {
+  out <- lchoose(size, x)
   return(out)
 }
 
@@ -27,14 +27,32 @@ calc_phi <- function() {
 #' The Binomial Distribution
 #'
 #' @inheritParams dnormalt
+#' @param size number of trials (zero or more).
 #' @return dbinomalt gives the density
 #' @inherit dnormalt source
 #' @examples
 #' library(altForm)
 #'
 #' set.seed(1)
-#' x <- rbinom(100, 1, .5) # Only contains zeros and ones
-#' dbinomalt(x, .5)
+#' x <- rbinom(100, 2, .5)
+#' dbinomalt(x, .5, 2)
 #'
 #' @export
-dbinomalt <- create_pdf_exponential_form(a, b, c2, link, calc_phi, FALSE, 0, 1, 0, 1)
+dbinomalt <- create_pdf_exponential_form_with_size(a, b, c2, link, calc_phi, 0)
+
+#' The Bernoulli Distribution
+#'
+#' @inheritParams dnormalt
+#' @return dbernalt gives the density
+#' @examples
+#' library(altForm)
+#'
+#' set.seed(1)
+#' x <- rbinom(100, 1, .5)
+#' dbernalt(x, .5)
+#'
+#' @export
+dbernalt <- function(x, mu, log = FALSE) {
+  out <- dbinomalt(x, mu, 1, log)
+  return(out)
+}
