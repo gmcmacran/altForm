37d147433bb3195e1aa68fb01cef859f21dc30ae
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-10-25 17:09:30 -0500

    cmf

--- R/binomial.R
+++ R/binomial.R
@@ -37,8 +37,15 @@ calc_phi <- function() {
 #' x <- rbinom(10, 2, .5)
 #' dbinomalt(x, .5, 2)
 #'
+#' pbinomalt(x, .5, 2)
+#'
 #' @export
 dbinomalt <- altForm:::create_pmf_exponential_form(a, b, c2, link, calc_phi, 0)
+
+#' @rdname dbinomalt
+#' @inheritParams pnormalt
+#' @export
+pbinomalt <- altForm:::create_cmf_exponential_form(dbinomalt, 0, Inf, 0, 1)
 
 #' The Bernoulli Distribution
 #'
@@ -51,8 +58,18 @@ dbinomalt <- altForm:::create_pmf_exponential_form(a, b, c2, link, calc_phi, 0)
 #' x <- rbinom(10, 1, .25)
 #' dbernalt(x, .25)
 #'
+#' pbernalt(x, .25)
+#'
 #' @export
 dbernalt <- function(x, mu, log = FALSE) {
   out <- dbinomalt(x, mu, 1, log)
+  return(out)
+}
+
+#' @rdname dbernalt
+#' @inheritParams pnormalt
+#' @export
+pbernalt <- function(x, mu, log = FALSE) {
+  out <- pbinomalt(x, mu, 1, log)
   return(out)
 }
