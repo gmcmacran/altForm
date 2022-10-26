a67d3278bd06a27e7efc9f48f7dc88d338264ebe
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-10-26 18:26:11 -0500

    cmf

--- tests/testthat/test_negative_binomial.R
+++ tests/testthat/test_negative_binomial.R
@@ -56,48 +56,74 @@ test_that("log input checking works", {
 })
 
 ###############################################
-# Density
+# cdf
 ###############################################
 test_that("Check structure.", {
-  expect_true(class(dgeomalt) == "function")
-  expect_true(all(names(formals(dgeomalt)) == c("x", "mu", "log")))
+  expect_true(class(pnbinomalt) == "function")
+  expect_true(all(names(formals(pnbinomalt)) == c("q", "mu", "size", "lower.tail", "log.p")))
 })
 
 for (mu in seq(.01, .99, .05)) {
-  set.seed(1)
-  x <- rgeom(100, mu)
+  for (n in c(1, 3, 5, 7, 9, 500, 9999)) {
+    set.seed(1)
+    q <- rnbinom(100, n, mu)
 
-  d1 <- round(dgeomalt(x, mu), 10)
-  d2 <- round(dgeom(x, mu), 10)
+    d1 <- round(pnbinomalt(q, mu, n), 10)
+    d2 <- round(pnbinom(q = q, size = n, prob = mu), 10)
 
-  d3 <- round(dgeomalt(x, mu, TRUE), 10)
-  d4 <- round(dgeom(x, mu, log = TRUE), 10)
+    d3 <- round(pnbinomalt(q, mu, n, TRUE, TRUE), 10)
+    d4 <- round(pnbinom(q = q, size = n, prob = mu, log.p = TRUE), 10)
 
-  test_that("Test results of density", {
-    expect_equal(d1, d2)
-    expect_equal(d3, d4)
-  })
+    test_that("Test results of cdf", {
+      expect_equal(d1, d2)
+      expect_equal(d3, d4)
+    })
+  }
+}
+
+# Total area is 1.
+for (mu in seq(.01, .99, .05)) {
+  for (n in c(1, 3, 5, 7, 9, 500, 9999)) {
+    d1 <- pnbinomalt(Inf, mu, n)
+
+    d3 <- pnbinomalt(Inf, mu, n, TRUE, TRUE)
+
+    test_that("Test results of cdf", {
+      expect_equal(d1, 1)
+      expect_equal(d3, 0)
+    })
+  }
 }
 
 ###############################################
-# Density Input checking
+# cdf Input checking
 ###############################################
 test_that("x input checking works", {
-  expect_error(dgeomalt(c()), "Argument x must have positive length.")
-  expect_error(dgeomalt(rep("foo", 50)), "Argument x must be numeric.")
-  expect_error(dgeomalt(rep(-10, 50)), "All elements in x must be greater than or equal to 0")
+  expect_error(pnbinomalt(c()), "Argument q must have positive length.")
+  expect_error(pnbinomalt(rep("foo", 50)), "Argument q must be numeric.")
+  expect_error(pnbinomalt(rep(-10, 50)), "All elements in q must be greater than or equal to 0")
 })
 
 set.seed(1)
-x <- rgeom(100, .5)
+q <- rnbinom(100, 10, .5)
 test_that("mu input checking works", {
-  expect_error(dgeomalt(x, c(1, 2)), "Argument mu must have length one.")
-  expect_error(dgeomalt(x, "foo"), "Argument mu must be numeric.")
-  expect_error(dgeomalt(x, 0), "Argument mu must be greater than zero.")
-  expect_error(dgeomalt(x, 1), "Argument mu must be less than one.")
+  expect_error(pnbinomalt(q, c(1, 2)), "Argument mu must have length one.")
+  expect_error(pnbinomalt(q, "foo"), "Argument mu must be numeric.")
 })
 
-test_that("log input checking works", {
-  expect_error(dgeomalt(x, .5, c(TRUE, FALSE)), "Argument log must have length one.")
-  expect_error(dgeomalt(x, .5, "foo"), "Argument log must be logical.")
+test_that("size input checking works", {
+  expect_error(pnbinomalt(q, .5, c(10, 20)), "Argument size must have length one.")
+  expect_error(pnbinomalt(q, .5, "foo"), "Argument size must be numeric.")
+  expect_error(pnbinomalt(q, .5, 0), "Argument size must be greater than 0.")
+  expect_error(pnbinomalt(q, .5, 10000), "Argument size must be less than 10,000.")
+})
+
+test_that("lower.tail input checking works", {
+  expect_error(pnbinomalt(q, .5, 10, c(TRUE, FALSE)), "Argument lower.tail must have length one.")
+  expect_error(pnbinomalt(q, .5, 10, "foo"), "Argument lower.tail must be logical.")
+})
+
+test_that("log.p input checking works", {
+  expect_error(pnbinomalt(q, .5, 10, TRUE, c(TRUE, FALSE)), "Argument log.p must have length one.")
+  expect_error(pnbinomalt(q, .5, 10, TRUE, "foo"), "Argument log.p must be logical.")
 })
